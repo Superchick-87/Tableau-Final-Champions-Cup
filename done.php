893a9e1cb158b6af4707081c45187871f161f7c2
Superchick-87 <nicolas.peyrebrune@gmail.com>
2024-04-08 18:40:53 +0200

    Front start

---
+++ done.php
@@ -0,0 +1,8 @@
+<?php
+// Récupérer les valeurs des inputs
+$input1 = $_POST['input1'];
+$input2 = $_POST['input2'];
+
+// Faire quelque chose avec les valeurs (par exemple, les afficher)
+echo "Valeur de input1 : " . $input1 . "<br>";
+echo "Valeur de input2 : " . $input2;
