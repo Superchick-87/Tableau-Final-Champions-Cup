d90a265ab71e7731669406246faa52e60ff018a1
Superchick-87 <nicolas.peyrebrune@gmail.com>
2024-04-09 19:03:30 +0200

    Maj

--- done.php
+++ done.php
@@ -1,8 +1,69 @@
 <?php
-// Récupérer les valeurs des inputs
-$input1 = $_POST['input1'];
-$input2 = $_POST['input2'];
+// Récupérer les données depuis $_POST
+$data = array(
+    "Quart de finale 1" => array(
+        $_POST['lieu_qf1'],
+        $_POST['team1_qf1'],
+        $_POST['score1_qf1'],
+        $_POST['team2_qf1'],
+        $_POST['score2_qf1']
+    ),
+    "Quart de finale 2" => array(
+        $_POST['lieu_qf2'],
+        $_POST['team1_qf2'],
+        $_POST['score1_qf2'],
+        $_POST['team2_qf2'],
+        $_POST['score2_qf2']
+    ),
+    "Quart de finale 3" => array(
+        $_POST['lieu_qf3'],
+        $_POST['team1_qf3'],
+        $_POST['score1_qf3'],
+        $_POST['team2_qf3'],
+        $_POST['score2_qf3']
+    ),
+    "Quart de finale 4" => array(
+        $_POST['lieu_qf4'],
+        $_POST['team1_qf4'],
+        $_POST['score1_qf4'],
+        $_POST['team2_qf4'],
+        $_POST['score2_qf4']
+    ),
+    "Demi-finale 1" => array(
+        $_POST['lieu_sf1'],
+        $_POST['team1_sf1'],
+        $_POST['score1_sf1'],
+        $_POST['team2_sf1'],
+        $_POST['score2_sf1']
+    ),
+    "Demi-finale 2" => array(
+        $_POST['lieu_sf2'],
+        $_POST['team1_sf2'],
+        $_POST['score1_sf2'],
+        $_POST['team2_sf2'],
+        $_POST['score2_sf2']
+    ),
+    "Finale" => array(
+        $_POST['lieu_final'],
+        $_POST['team1_final'],
+        $_POST['score1_final'],
+        $_POST['team2_final'],
+        $_POST['score2_final']
+    )
+);
 
-// Faire quelque chose avec les valeurs (par exemple, les afficher)
-echo "Valeur de input1 : " . $input1 . "<br>";
-echo "Valeur de input2 : " . $input2;
+// Ouvrir ou créer le fichier CSV en mode écriture
+$file = fopen('datas.csv', 'w');
+
+// Parcourir chaque élément du tableau et écrire dans le fichier CSV
+foreach ($data as $key => $value) {
+    fputcsv($file, array_merge(array($key), $value));
+}
+
+// Fermer le fichier
+fclose($file);
+
+// Réponse indiquant que les données ont été écrites avec succès dans le fichier CSV
+echo "Les données ont été enregistrées dans le fichier CSV avec succès.";
+?>
+
